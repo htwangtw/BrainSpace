2e8c642c58bc864aa3d831515362e27cc0653ffb
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2022-01-04 10:37:29 -0500

    Version bump, minor docstring fix

--- matlab/surface_manipulation/full2parcel.m
+++ matlab/surface_manipulation/full2parcel.m
@@ -1,7 +1,7 @@
 function parcel_data = full2parcel(data,parcellation)
 % FULL2PARCEL   Downsamples vertex data to parcel data.
 %
-%   parcel_Data = full2parcel(data,parcellation) takes the mean of columns
+%   parcel_data = full2parcel(data,parcellation) takes the mean of columns
 %   in n-by-m matrix data that have the same value in corresponding 1-by-m
 %   vector parcellation. If some numbers are missing in the parcellation
 %   vector, these are returned as NaN columns.
